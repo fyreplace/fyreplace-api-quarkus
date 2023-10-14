cb4dee1739573ee93ace18db9412fe0dcc2d5b82
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-10-14 13:44:47 +0200

    Allow public reads on bucket

--- src/main/java/app/fyreplace/api/services/storage/s3/S3StorageService.java
+++ src/main/java/app/fyreplace/api/services/storage/s3/S3StorageService.java
@@ -1,12 +1,17 @@
 package app.fyreplace.api.services.storage.s3;
 
 import app.fyreplace.api.services.StorageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.arc.Unremovable;
 import io.quarkus.arc.properties.IfBuildProperty;
+import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import org.jboss.logging.Logger;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -23,6 +28,25 @@ public final class S3StorageService implements StorageService {
 
     @Inject
     S3Client client;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    public void onStartup(@Observes final StartupEvent event) {
+        client.putBucketPolicy(b -> {
+            final var statement = new Policy.Statement();
+            statement.Effect = "Allow";
+            statement.Principal = "*";
+            statement.Action = "s3:GetObject";
+            statement.Resource = "arn:aws:s3:::" + config.bucket() + "/*";
+
+            try {
+                b.bucket(config.bucket()).policy(objectMapper.writeValueAsString(new Policy(List.of(statement))));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     @Override
     public void store(final String path, final byte[] data) {
